db18ae5ddb5ca2c81fe2e74ad1672367c0b611e4
mercylin <linmercymuthoni@gmail.com>
2024-03-02 17:13:19 -0800

    json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python3
 """Defines the Base class."""
+
+import json
+
 class Base:
     """Represents the base class."""
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initializes a new instance of the Base class."""
+        """Initializes a new Base instance."""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries."""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
 
