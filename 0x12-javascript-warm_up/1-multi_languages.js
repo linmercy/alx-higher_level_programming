b872931f6e81ec4ea954f9dd4e4c25d8405889e3
mercylin <linmercymuthoni@gmail.com>
2024-04-09 23:45:08 -0700

    multi languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const cProgramming = 'C is fun';
+const pythonProgramming = 'Python is cool';
+const javascriptProgramming = 'Javascript is amaizing';
+
+console.log(cProgramming);
+console.log(pythonProgramming);
+console.log(javascriptProgramming);
